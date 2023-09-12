fca63f0987de56ccd03dd98f44e1a31143bc70b6
a.arkhipovdev <a.arkhipovdev@gmail.com>
2023-09-12 13:31:45 +0300

    feat: import movies, fix bugs

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {QueryClientProvider} from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-import {queryClient} from './src/services/api/queryClient';
-import {Provider} from 'react-redux';
-import {persistor, store} from './src/store';
-import {PersistGate} from 'redux-persist/integration/react';
-import {RootNavigation} from './src/navigation/RootNavigation';
+import { queryClient } from './src/services/api/queryClient';
+import { Provider } from 'react-redux';
+import { persistor, store } from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { RootNavigation } from './src/navigation/RootNavigation';
+import Toast from 'react-native-toast-message';
 
 if (__DEV__) {
-  import('react-query-native-devtools').then(({addPlugin}) => {
-    addPlugin({queryClient});
+  import('react-query-native-devtools').then(({ addPlugin }) => {
+    addPlugin({ queryClient });
   });
 }
 
@@ -19,6 +20,7 @@ function App(): JSX.Element {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <RootNavigation />
+          <Toast />
         </PersistGate>
       </Provider>
     </QueryClientProvider>
